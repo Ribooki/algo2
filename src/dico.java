1d8a55e458263ddea740f46369c4553d550ea845
E220397 <elias.munoz@outlook.fr>
2018-10-17 10:37:16 +0200

    J'ai update la lecture de fichier et la création d'un dico.

--- src/dico.java
+++ src/dico.java
@@ -1,5 +1,34 @@
-public class dico {
-    public dico(String filename){
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
 
+public class dico {
+    private HashSet<String> h;
+    public dico(FileInputStream f){
+        h = Read_file(f);
+    }
+
+    public HashSet<String> getH() {
+        return h;
+    }
+
+    private HashSet<String> Read_file(FileInputStream f) {
+        HashSet<String> wl = new HashSet<>();
+        try {
+            final BufferedReader reader = new BufferedReader(new InputStreamReader(f));
+            while (reader.ready()) {
+                wl.add(reader.readLine());
+            }
+
+            return wl;
+
+
+        }
+        catch(IOException e){
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 }
