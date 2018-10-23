5b1dd0179ee7da5b13380536794bc616251cb340
ribooki <rallemannd@gmail.com>
2018-10-23 17:03:10 +0200

    delet class trigramme

--- src/trigrammes.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
-public class trigrammes {
-    private Map<String, ArrayList<String>> trigrammes = new HashMap<>();
-
-}
