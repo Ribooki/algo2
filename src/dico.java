1e456fa660d4adc0df886eabedf469ecd8f2d44b
ribooki <rallemannd@gmail.com>
2018-10-23 17:21:14 +0200

    Ajout de la création des trigrammes à partir du dico.

--- src/dico.java
+++ src/dico.java
@@ -8,23 +8,24 @@ import java.util.HashSet;
 import java.util.Map;
 
 public class dico {
-    private static HashSet<String> h;
+    private HashSet<String> h;
     private Map<String, ArrayList<String>> trigrammes = new HashMap<>();
 
     public dico(FileInputStream f){
         h = readFile(f);
+        createTrigrammesDico();
     }
 
     public HashSet<String> getH() {
         return h;
     }
 
-    public static boolean dicoVide(){
+    public boolean dicoVide(){
         return h.isEmpty();
     }
 
-    public static boolean mExiste(String M){
-        return h.contains(M);
+    public boolean mExiste(String M){
+        return h.contains("<" + M + ">");
     }
 
     private HashSet<String> readFile(FileInputStream f) {
@@ -40,5 +41,28 @@ public class dico {
             System.out.println(e.getMessage());
             return null;
         }
+    }
+
+    private void createTrigrammesDico(){
+        ArrayList<String> dico = new ArrayList<String>(h);
+        for(String mot : dico){
+            ArrayList<String> trigrammesMot = createTrigrammesMot(mot);
+            for(String trig : trigrammesMot){
+                if(!trigrammes.containsKey(trig)){
+                    trigrammes.put(trig, new ArrayList<String>());
+                }
+                trigrammes.get(trig).add(mot);
+            }
+        }
+    }
+
+    private ArrayList<String> createTrigrammesMot(String mot){
+        ArrayList<String> t = new ArrayList<String>();
+        String trigramme;
+        for(int i=0 ; i+3<mot.length() ; i++){
+            trigramme = mot.substring(i, i+3);
+            t.add(trigramme);
+        }
+        return t;
     }
 }
